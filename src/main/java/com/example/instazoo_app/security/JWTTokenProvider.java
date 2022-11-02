1bdbe6e3e4236e0cebd56a436301c90200f63dea
Dmitriy-Dolbik <dda1508@gmail.com>
2022-11-02 11:06:45 +0300

    By Alishev Completely

--- src/main/java/com/example/instazoo_app/security/JWTTokenProvider.java
+++ src/main/java/com/example/instazoo_app/security/JWTTokenProvider.java
@@ -3,12 +3,10 @@ package com.example.instazoo_app.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.instazoo_app.models.User;
 import com.example.instazoo_app.repositories.UsersRepository;
-import io.jsonwebtoken.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -16,54 +14,39 @@ import org.springframework.stereotype.Component;
 
 import java.time.ZonedDateTime;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class JWTTokenProvider {
     @Value("${jwt_secret}")
     private String secret;
-    public static final Logger LOG = LoggerFactory.getLogger(JWTTokenProvider.class);
+    private final UsersRepository usersRepository;
+    @Autowired
+    public JWTTokenProvider(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
 
-    public String generateToken(Authentication authentication) {
+    public String generateToken(Authentication authentication){
         User user = (User) authentication.getPrincipal();
-        Date expiryDate = Date.from(ZonedDateTime.now().plusMinutes(10).toInstant());
+        Date expirationDate = Date.from(ZonedDateTime.now().plusMinutes(10).toInstant());
 
         String userId = Long.toString(user.getId());
-
-        Map<String, Object> claimsMap = new HashMap<>();
-        claimsMap.put("id", userId);
-        claimsMap.put("username", user.getEmail());
-
-        return Jwts.builder()
-                .setSubject(userId)
-                .addClaims(claimsMap)
-                .setIssuedAt(new Date())
-                .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, secret)
-                .compact();
+        return JWT.create()
+                .withSubject("User details")
+                .withClaim("id", userId)
+                .withClaim("username", user.getEmail())
+                .withIssuedAt(new Date())
+                .withIssuer("instazoo")
+                .withExpiresAt(expirationDate)
+                .sign(Algorithm.HMAC256(secret));
     }
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser()
-                    .setSigningKey(secret)
-                    .parseClaimsJws(token);
-            return true;
-        }catch (SignatureException |
-                MalformedJwtException |
-                ExpiredJwtException |
-                UnsupportedJwtException |
-                IllegalArgumentException ex) {
-            LOG.error(ex.getMessage());
-            return false;
-        }
-    }
-    public Long getUserIdFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(secret)
-                .parseClaimsJws(token)
-                .getBody();
-        String id = (String) claims.get("id");
-        return Long.parseLong(id);
+    public Long getUserIdFromToken(String token) throws JWTVerificationException {
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
+                .withSubject("User details")
+                .withIssuer("instazoo")
+                .build();
+
+        DecodedJWT jwt = verifier.verify(token);
+        Long userId = Long.parseLong(jwt.getClaim("id").asString());
+        return userId;
     }
 }
