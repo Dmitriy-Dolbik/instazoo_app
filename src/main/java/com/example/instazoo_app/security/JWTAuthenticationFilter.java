54b88410a6d5100fc6a0f8868be60d2b688bfccf
Dmitriy-Dolbik <dda1508@gmail.com>
2022-11-04 20:57:25 +0300

    Refactoring JWTAuthenticationFilter. response.sendError doesn't work.
    response in catch destroy correct response from JWTAuthenticationEntryPoint

--- src/main/java/com/example/instazoo_app/security/JWTAuthenticationFilter.java
+++ src/main/java/com/example/instazoo_app/security/JWTAuthenticationFilter.java
@@ -34,36 +34,60 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-            /*if (jwt.isBlank()) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && !authHeader.isBlank() && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7);
+
+            if (jwt.isBlank()) {
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED
                         , "Invalid JWT Token in Bearer Header");
-            } else {*/
-        try {
-            String jwt = getJWTFromRequest(request);
-            if (!jwt.isBlank()) {
-                Long userId = jwtTokenProvider.getUserIdFromToken(jwt);
-                User userDetails = customUserDetailsService.loadUserById(userId);
+            } else {
+                try {
+                    Long userId = jwtTokenProvider.getUserIdFromToken(jwt);
+                    User userDetails = customUserDetailsService.loadUserById(userId);
 
-                UsernamePasswordAuthenticationToken authToken =
-                        new UsernamePasswordAuthenticationToken(
-                                userDetails
-                                , userDetails.getPassword()
-                                , userDetails.getAuthorities());
-                if (SecurityContextHolder.getContext().getAuthentication() == null) {
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                    UsernamePasswordAuthenticationToken authToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails
+                                    , userDetails.getPassword()
+                                    , userDetails.getAuthorities());
+                    if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
+                } catch (JWTVerificationException exs) {
+                    LOG.error("Could not set user authentication");
                 }
             }
-        } catch (
-                Exception exs) {
-            LOG.error("Could not set user authentication");
         }
         filterChain.doFilter(request, response);
     }
+    //рефакторинг с отдельным методом для доставания JWT из хэдера
+     /*try {
+        String jwt = getJWTFromRequest(request);
+        if (!jwt.isBlank()) {
+            Long userId = jwtTokenProvider.getUserIdFromToken(jwt);
+            User userDetails = customUserDetailsService.loadUserById(userId);
+
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(
+                            userDetails
+                            , userDetails.getPassword()
+                            , userDetails.getAuthorities());
+            if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+    } catch (
+    Exception exs) {
+        LOG.error("Could not set user authentication");
+    }
+        filterChain.doFilter(request, response);
+}
     private String getJWTFromRequest(HttpServletRequest request) {
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && !authHeader.isBlank() && authHeader.startsWith("Bearer ")) {
             return authHeader.substring(7);
         }
         return null;
-    }
+    }*/
 }
